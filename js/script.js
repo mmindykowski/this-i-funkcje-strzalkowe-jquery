8730d9cae70420b15589a578e68353d6d93d661f
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-05-03 21:59:17 +0200

    jquery funkcje strzalkowe i this

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('#click-button').click(function() {
+        // this.style.backgroundColor = 'green';
+        $(this).css('background-color', 'green');
+    });
+
+    $('#click-button').click((e)=> {
+        $(e.target).css('color', 'yellow');
+    });
+
+
+});
